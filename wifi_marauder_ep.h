0558fabddb80599250ea1ec2443525c159151137
0xchocolate <109879152+0xchocolate@users.noreply.github.com>
2023-11-17 23:29:01 -0800

    Fixup, run format

--- wifi_marauder_ep.h
+++ wifi_marauder_ep.h
@@ -3,4 +3,4 @@
 
 #include "wifi_marauder_app_i.h"
 
-bool wifi_marauder_ep_read_html_file(WifiMarauderApp* app, uint8_t* the_html, size_t* html_size);
+bool wifi_marauder_ep_read_html_file(WifiMarauderApp* app, uint8_t** the_html, size_t* html_size);
